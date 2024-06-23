21607be56176cba0adfc6eb902f6dadc51a79388
Christo Swanepoel <xtoswanepoel@gmail.com>
2024-06-23 11:17:19 +0200

    feat: added models

---
+++ Simple_API_Assessment/Models/Skill.cs
@@ -0,0 +1,9 @@
+﻿namespace Simple_API_Assessment.Models
+{
+  public class Skill
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    // TODO: Add ApplicantId relation
+  }
+}
