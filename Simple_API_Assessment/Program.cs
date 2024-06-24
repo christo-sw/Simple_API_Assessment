20105f7524dab1371b3d241649dd51ce9b925fb2
Christo Swanepoel <xtoswanepoel@gmail.com>
2024-06-24 22:44:25 +0200

    feat: data seeding

--- Simple_API_Assessment/Program.cs
+++ Simple_API_Assessment/Program.cs
@@ -1,25 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using Simple_API_Assessment;
+using Simple_API_Assessment.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddTransient<Seed>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// TODO: Remove this injection. It should be constructed manually in each repo
+builder.Services.AddDbContext<DataContext>(options =>
+{
+  options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
+
+// Seed the database
+var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+using (var scope = scopedFactory.CreateScope())
+{
+  var service = scope.ServiceProvider.GetService<Seed>();
+  service.SeedDataContext();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
---
+++ Simple_API_Assessment/Seed.cs
@@ -0,0 +1,50 @@
+﻿using Simple_API_Assessment.Data;
+using Simple_API_Assessment.Models;
+
+namespace Simple_API_Assessment
+{
+  public class Seed
+  {
+    private readonly DataContext _dataContext;
+
+    public Seed(DataContext context)
+    {
+      _dataContext = context;
+    }
+
+    public void SeedDataContext()
+    {
+      if (!_dataContext.Applicants.Any() && !_dataContext.Skills.Any())
+      {
+        var skills = new List<Skill>()
+        {
+          new Skill()
+          {
+            Id = 1,
+            Name = "Programming"
+          },
+          new Skill()
+          {
+            Id = 2,
+            Name = "Problem solving"
+          },
+          new Skill()
+          {
+            Id = 3,
+            Name = "Tenacity"
+          }
+        };
+
+        var applicant = new Applicant()
+        {
+          Id = 1,
+          Name = "Christo Swanepoel",
+          Skills = skills
+        };
+
+        _dataContext.Applicants.Add(applicant);
+        _dataContext.SaveChanges();
+      }
+    }
+  }
+}
